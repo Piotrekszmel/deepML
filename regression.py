d05053a19de0823e2518685079ee2218a90f35ad
Piotrekszmel <piotr.szmelczynski@gmail.com>
2020-03-14 15:46:52 +0100

    merge Linear class with MultiLinear

--- regression.py
+++ regression.py
@@ -1,25 +1,28 @@
 import matplotlib.pyplot as plt
 import numpy as np 
-#import pandas as pd
+import pandas as pd
 from math import sqrt
-#from sklearn.datasets import make_regression
+from sklearn.datasets import make_regression
 
 np.random.seed(42)
 
 
 class Linear:
     def __init__(self, X, y, scale=0, lr=0.005, verbose=0):
+        X = np.array(X)
         self.X = self.standardize(X) if scale == 1 else X
         self.y = y
         self.scale = scale
+        self.m = X.shape[1] if X.ndim >= 2 else 1
         self.n = len(y)
         self.lr = lr
         self.verbose = verbose
-        self.theta0 = np.random.rand()
-        self.theta1 = np.random.rand()
+        self.theta = np.random.randn(X.shape[1] + 1) if X.ndim >= 2 else np.random.randn(2)
     
-    def hypothesis(self, x):
-        return self.theta0 + (self.theta1 * x)
+    def hypothesis(self, X):
+        if X.ndim == 0:
+            X = X.reshape([1,1])
+        return self.theta[0] + np.matmul(X, self.theta[1:])
 
     def standardize(self, X):
         X = X - np.mean(X) 
@@ -35,44 +38,57 @@ class Linear:
         return sqrt(mean_error)
 
     def cost(self):
-        J = 0
-        for xi, yi in zip(self.X, self.y):
-            J += ((self.hypothesis(xi) - yi)**2) / (2 * self.n)
-        return J
+        self.J = 0
+        for xs, ys in zip(self.X, self.y):
+            h = self.hypothesis(xs)
+            self.J += (1 / (2 * self.m)) * np.sum((h - ys)**2)
+        return self.J
     
     def derivatives(self):
         dtheta0 = 0
-        dtheta1 = 0
+        dtheta = 0
         for xi, yi in zip(self.X, self.y):
             dtheta0 += self.hypothesis(xi) - yi
-            dtheta1 += (self.hypothesis(xi) - yi) * xi
+            dtheta += (self.hypothesis(xi) - yi) * xi
 
         dtheta0 /= self.n
-        dtheta1 /= self.n
+        dtheta /= self.n
 
-        return dtheta0, dtheta1
+        return dtheta0, dtheta
     
     def updateParameters(self):
-        dtheta0, dtheta1 = self.derivatives()
-        self.theta0 = self.theta0 - (self.lr * dtheta0)
-        self.theta1 = self.theta1 - (self.lr * dtheta1)
+        dtheta0, dtheta = self.derivatives()
+        self.theta[0] = self.theta[0] - (self.lr / self.m) * dtheta0
+        self.theta[1:] = self.theta[1:] - (self.lr / self.m) * dtheta
 
+    def train(self, num_iter):
+        self.num_iter = num_iter
+        self.cost_history = []
+        self.theta_history = []
+        for i in range(num_iter):
+            self.cost_history.append(self.cost())
+            self.theta_history.append(self.theta)
+            self.updateParameters()
+        if self.verbose == 1:
+            self.plotLine(self.X, self.y, self.theta[0], self.theta[1:])                                                  
+
+        return self.theta, self.cost_history, self.theta_history
+    
     def predict(self, X_test):
-        if self.scale == 1:
-            X_test = self.standardize(X_test)
-        predictions = []
-        for x in X_test:
-            predictions.append(self.hypothesis(x))
-        return predictions
+        X_test = np.array(X_test)
+        self.Y_hat = []
+        for xs in X_test:
+            self.Y_hat.append(self.hypothesis(xs))
+        return self.Y_hat
 
-    def plotLine(self, X, y, theta0, theta1):
+    def plotLine(self, X, y, theta0, theta):
         max_x = np.max(X) + np.max(X) * 0.2
         min_x = np.min(X) - np.min(X) * 0.2
         max_y = np.max(y) + np.max(y) * 0.2
         min_y = np.min(y) - np.min(y) * 0.2
 
-        xplot = np.linspace(min_x, max_x, 1000)
-        yplot = theta0 + theta1 * xplot
+        xplot = np.linspace(min_x, max_x, 5000)
+        yplot = theta0 + theta * xplot
 
         plt.plot(xplot, yplot, color='#ff0000', label='Regression Line')
 
@@ -80,100 +96,21 @@ class Linear:
         plt.axis([min_x * 1.2, max_x * 1.2, min_y * 1.2, max_y * 1.2])
         plt.show()
 
-    def train(self, num_iter):
-        cost_history = [0] * num_iter
-        for i in range(num_iter):
-            self.updateParameters()
-            cost_history[i] = self.cost()
-        if self.verbose == 1:
-            self.plotLine(self.X, self.y, self.theta0, self.theta1)                                                  
-
-        return cost_history
+    def plotCost(self, cost_h, num_iter):
+        plt.plot(list(range(num_iter)), cost_h, "-r")
+        plt.show()
 
 
-class MultipleLinear:
-    def __init__(self, X, y, lr):
-        X = np.array(X)
-        self.X = X
-        self.y = y
-        self.m = X[0].shape if X.ndim >= 2 else 1
-        self.n = X[1].shape if X.ndim >= 2 else 1
-        self.lr = lr
-        self.theta = np.random.randn(X.shape[1] + 1) if X.ndim >= 2 else np.random.randn(2)
-    
-    def hypothesis(self, X):
-        if X.ndim == 0:
-            X = X.reshape([1,1])
-        return self.theta[0] + np.matmul(X, self.theta[1:])
+X, y = make_regression(n_samples=10, n_features=3, noise=0.4, bias=50)
 
-    def cost(self):
-        self.J = 0
-        for xs, ys in zip(self.X, self.y):
-            h = self.hypothesis(xs)
-            self.J += (1 / (2 * self.m)) * np.sum((h - ys)**2)
-        return self.J
-    
-    def derivatives(self, X, y):
-        value = 0
-        theta0 = 0
-        for xs, ys in zip(X, y):
-            theta0 += (self.hypothesis(xs) - ys)
-            value += (self.hypothesis(xs) - ys) * xs
-        return value, theta0
-
-
-    def train(self, num_iter=1000, lr=0.005):
-        self.cost_history = []
-        self.theta_history = []
-        for i in range(num_iter):
-            self.cost_history.append(self.cost())
-            self.theta_history.append(self.theta)
-            sum1, sum0 = self.derivatives(self.X, self.y)
-            self.theta[0] = self.theta[1:] - (self.lr / self.m) * sum0
-            self.theta[1:] = self.theta[1:] - (self.lr / self.m) * sum1
-        
-        return self.theta, self.cost_history, self.theta_history 
-
-    def predict(self, X_test, Y_test):
-        self.Y_hat = self.hypothesis(X_test)
-        return self.Y_hat
-    
-
-#X, Y = make_regression(n_samples=100, n_features=1, noise=0.4, bias=50)
-X = [1,2,3,4,5]
-y = [3,4,5,6,7]
-
-mLinear = MultipleLinear(X, y, 0.005)
-theta, cost_h, theta_h = mLinear.train(1000)
-
+#X = [[1,2,3,4,5], [10,11,12,13,14]]
+#y = [6, 15]
 linear = Linear(X, y, scale=0, verbose=0)
-cost = linear.train(1000)
+theta, cost_h, theta_h = linear.train(2000)
 
-print(cost_h[-3:])
-print(cost[-3:])
-
-"""
-X_test = [6,700, 8,9000]
-Y_test = [8,9,10,11]
-predictions = linear.predict(X_test)
-
-print(linear.rmse_metric(Y_test, predictions))
-
-linear = Linear(X, Y, scale=1)
-linear.train()
-
-X_test = [6,700,8,9000]
-Y_test = [8,9,10,11]
-
-
-predictions = linear.predict(X_test)
-print(linear.rmse_metric(Y_test, predictions))
-
-
-
-print("Actual: ", Y_test)
-print("Predictions: ", predictions)
-"""
+linear.plotCost(cost_h, linear.num_iter)
+#predictions = linear.predict([[6,7,8,9,10]])
+#print(predictions)
 
 
 
