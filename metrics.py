990f54e14bfd1f84be87036a187b0bd441c84071
Piotrekszmel <piotr.szmelczynski@gmail.com>
2020-03-16 16:38:46 +0100

    create __repr__ method for confusion matrix

--- metrics.py
+++ metrics.py
@@ -9,7 +9,9 @@ class confusion_matrix:
         self.K = len(labels) if labels else len(np.unique(y_true))
         self.cm = self.make_matrix()
         self.acc = self.accuracy(self.cm)
-    
+        self.rec = self.recall(self.cm)
+        self.prec = self.precision(self.cm)
+
     def make_matrix(self):
         self.cm = np.zeros((self.K, self.K))
         for i in range(len(self.y_true)):
@@ -21,9 +23,28 @@ class confusion_matrix:
         denominator = np.sum(cm)
         self.acc = numerator / denominator
         return self.acc
+
+    def recall(self, cm):
+        numerator = cm[1, 1]
+        denominator = np.sum([cm[1, 0], cm[1, 1]])
+        if denominator == 0:
+            self.rec = 0.0
+            return self.rec
+        self.rec = numerator / denominator
+        return self.rec
+    
+    def precision(self, cm):
+        numerator = cm[1, 1]
+        denominator = np.sum([cm[0, 1], cm[1, 1]])
+        if denominator == 0:
+                self.prec = 0.0
+                return self.prec
+        self.prec = numerator / denominator
+        return self.prec
     
     def __repr__(self):
-        return f"labels: {self.labels} \n\n {self.cm}  \n\n Accuracy: {self.acc}"
+        return (f" labels: {self.labels} \n\n matrix: \n {self.cm} \n\n accuracy: {self.acc} \n" + 
+        f" recall: {self.rec} \n precision: {self.prec} \n")
 
-confusion_m = confusion_matrix([0,0,0,0], [0,0,0,1], labels=[0, 1])
+confusion_m = confusion_matrix([1,1,0,0], [0,1,1,0], labels=[0, 1])
 print(confusion_m)
