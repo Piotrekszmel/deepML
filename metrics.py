8913dd15e65416619360b23247213d9e2d56fc58
Piotrekszmel <piotr.szmelczynski@gmail.com>
2020-03-16 15:55:24 +0100

    create accuracy for confusion_matrix

--- metrics.py
+++ metrics.py
@@ -2,10 +2,13 @@ import numpy as np
 
 
 class confusion_matrix:
-    def __init__(self, y_true, y_pred):
+    def __init__(self, y_true, y_pred, labels=None):
         self.y_true = y_true
         self.y_pred = y_pred
-        self.K = len(np.unique(y_true))
+        self.labels = labels
+        self.K = len(np.unique(y_true)) if labels == None else len(labels)
+        self.cm = self.make_matrix()
+        self.acc = self.accuracy(self.cm)
     
     def make_matrix(self):
         self.cm = np.zeros((self.K, self.K))
@@ -13,9 +16,15 @@ class confusion_matrix:
             self.cm[self.y_true[i], self.y_pred[i]] += 1
         return self.cm
     
-    def precision(self)
+    def accuracy(self, cm):
+        numerator = np.sum([cm[0, 0], cm[1, 1]] )
+        denominator = np.sum(cm)
+        self.acc = numerator / denominator
+        return self.acc
+    
 
-
-confusion_m = confusion_matrix([1,0,0,1], [0,0,0,1])
-cm = confusion_m.make_matrix()
-print(cm)
+confusion_m = confusion_matrix([1,0,0,0], [0,0,0,1], labels=[True,False])
+print(confusion_m.cm)
+print()
+print(confusion_m.acc)
+print(confusion_m.labels)
