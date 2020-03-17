9b40f9f353afe4aafd740a38cf951091300b8fa6
Piotrekszmel <piotr.szmelczynski@gmail.com>
2020-03-17 15:06:41 +0100

    add _best_split method for DecisionTreeClassifier

--- tree.py
+++ tree.py
@@ -17,9 +17,51 @@ class DecisionTreeClassifier:
     def fit(self, X, y):
         self.n_classes = len(set(y))
         self.n_features = X.shape[1]
-        self.tree = self._grow_tree(X, y)
+        #self.tree = self._grow_tree(X, y)
 
     def predict(self, X):
         return [self._predict(inputs) for inputs in X]
     
-    
+    def _best_split(self, X, y):
+        m = y.size
+        if m <= 1:
+            return None, None
+        num_parent = [np.sum(y == c) for c in range(self.n_classes)]
+        best_gini = 1.0 - sum((n / m) ** 2 for n in num_parent)
+        best_idx, best_thr = None, None
+        for idx in range(self.n_features):
+            thresholds, classes = zip(*sorted(zip(X[:, idx], y)))
+            num_left = [0] * self.n_classes
+            num_right = num_parent.copy()
+
+            for i in range(1, m):
+                c = classes[i - 1]
+                num_left[c] += 1
+                num_right[c] -= 1
+                gini_left = 1.0 - sum(
+                    (num_left[x] / i) ** 2 for x in range(self.n_classes)
+                    )
+                gini_right = 1.0 - sum(
+                    (num_right[x] / (m - i)) ** 2 for x in range(self.n_classes)
+                    )
+                gini = (i * gini_left + (m - i) * gini_right) / m
+                if thresholds[i] == thresholds[i - 1]:
+                    continue
+                if gini < best_gini:
+                    best_gini = gini
+                    best_idx = idx
+                    best_thr = (thresholds[i] + thresholds[i - 1]) / 2
+        return best_idx, best_thr
+
+
+
+
+
+
+
+
+
+
+tree = DecisionTreeClassifier(max_depth=1)
+tree.fit(np.array([[1,2,3], [1,2,3], [1,2,3]]), np.array([0,1,2]))
+tree._best_split(np.array([[1,"blue",7], [1,"green",3], [1,"yellow",3]]), np.array([0,0,2]))
