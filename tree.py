4f1ed8b558e51b9be42257dd3f17b6dec878aadd
Piotrekszmel <piotr.szmelczynski@gmail.com>
2020-03-17 13:57:43 +0100

    create DecisionTreeClassifier class

--- tree.py
+++ tree.py
@@ -10,3 +10,16 @@ class Node:
         self.right = None
 
     
+class DecisionTreeClassifier:
+    def __init__(self, max_depth=None):
+        self.max_depth = max_depth
+
+    def fit(self, X, y):
+        self.n_classes = len(set(y))
+        self.n_features = X.shape[1]
+        self.tree = self._grow_tree(X, y)
+
+    def predict(self, X):
+        return [self._predict(inputs) for inputs in X]
+    
+    
