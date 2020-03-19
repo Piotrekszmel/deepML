ea74178f7917360a366d4fe0791638aca871367f
Piotrekszmel <piotr.szmelczynski@gmail.com>
2020-03-19 19:14:34 +0100

    change np.array to list

--- naive_bayes.py
+++ naive_bayes.py
@@ -1,4 +1,4 @@
-import numpy as np 
+from math import sqrt
 
 
 class GaussianNB:
@@ -6,12 +6,12 @@ class GaussianNB:
         pass
 
     def mean(self, numbers):
-        return np.sum(numbers) / float(len(numbers))
+        return sum(numbers) / float(len(numbers))
     
     def stdev(self, numbers):
         avg = self.mean(numbers)
-        variance = np.sum([(x - avg)**2 for x in numbers]) / float(len(numbers - 1))
-        return np.sqrt(variance)
+        variance = sum([(x-avg)**2 for x in numbers]) / float(len(numbers)-1)
+        return sqrt(variance)
     
     def separate_by_class(self, X, y):
         separated = dict()
@@ -22,7 +22,7 @@ class GaussianNB:
         return separated
     
     def summarize_dataset(self, X):
-        summaries = [(self.mean(col), self.stdev(col), len(col)) for col in X[:]]
+        summaries = [(self.mean(col), self.stdev(col), len(col)) for col in zip(*X)]
         return summaries
     
     def summarize_by_class(self, X, y):
@@ -33,10 +33,25 @@ class GaussianNB:
         return summaries
     
 gnb = GaussianNB()
-X, y = np.array([[1,2], [3,4], [5,6], [7,8]]), [1, 2, 3, 1]
-separated = gnb.separate_by_class(X, y)
+
+l = [[3.393533211,2.331273381],
+	[3.110073483,1.781539638],
+	[1.343808831,3.368360954],
+	[3.582294042,4.67917911],
+	[2.280362439,2.866990263],
+	[7.423436942,4.696522875],
+	[5.745051997,3.533989803],
+	[9.172168622,2.511101045],
+	[7.792783481,3.424088941],
+	[7.939820817,0.791637231]]
+m = [0,0,0,0,0,1,1,1,1,1]
+#summ = gnb.summarize_dataset(l)
+
+separated = gnb.summarize_by_class(l, m)
+
 print(separated)
-summ = gnb.summarize_dataset(X)
+"""
 print("\n", summ)
-summaries = gnb.summarize_by_class(X, y)
+summaries = gnb.summarize_by_class(l, m)
 print("\n", summaries)
+"""
