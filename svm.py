353aa689e583d44183102d5fb4880504108d5406
Piotrekszmel <piotr.szmelczynski@gmail.com>
2020-03-21 20:45:27 +0100

    add docstring to SVM

--- svm.py
+++ svm.py
@@ -2,75 +2,114 @@ import matplotlib.pyplot as plt
 import numpy as np 
 
 
-class SVM:
-    def __init__(self, visualization=True):
+class SVM(object):
+    """
+    Support vector Machine with Linear kernel.
+
+     Example: 
+    
+    ```python
+        data_dict = {-1: np.array([[1,7],[2,8],[3,8]]),1: np.array([[5,1],[6,-1],[7,3]])}
+
+        svm = SVM()
+        svm.fit(data=data_dict)
+        svm.visualize()
+    
+    @param: visualization (bool) : if true create plot 
+    
+    @fit: find best W and b
+    @predict: return predicted value
+    """
+
+    def __init__(self,visualization=True):
         self.visualization = visualization
-        self.colors = {1: "r", -1: "b"}
+        self.colors = {1:'r',-1:'b'}
         if self.visualization:
             self.fig = plt.figure()
             self.ax = self.fig.add_subplot(1,1,1)
-        
-    def fit(self, X, y):
-        self.X = X
-        self.y = y
+    
+    def fit(self,data):
+        self.data = data
         opt_dict = {}
-
-        transforms = [[1,1],[-1,1],[-1,-1],[1,-1]]
-
-        data = np.array([])
-
-        for xi in self.X:
-            data = np.append(data, xi)
         
-        self.max_feature_value = max(data)
-        self.min_feature_value = min(data)
-        data = None
-
-        step_sizes = [self.max_feature_value * 0.01,
+        transforms = [[1,1],[-1,1],[-1,-1],[1,-1]]
+        
+        all_data = np.array([])
+        for yi in self.data:
+            all_data = np.append(all_data,self.data[yi])
+                    
+        self.max_feature_value = max(all_data)         
+        self.min_feature_value = min(all_data)
+        all_data = None
+        
+        step_sizes = [self.max_feature_value * 0.1,
                       self.max_feature_value * 0.01,
-                      self.max_feature_value * 0.001]
+                      self.max_feature_value * 0.001,]
         
         b_range_multiple = 5
         b_multiple = 5
-
+        
         latest_optimum = self.max_feature_value * 10
-
+        
         for step in step_sizes:
-            w = np.array([latest_optimum, latest_optimum])
-
+            w = np.array([latest_optimum,latest_optimum])
+            
             optimized = False
             while not optimized:
-                for b in np.arange(-1 * self.max_feature_value * b_range_multiple,
-                                   self.max_feature_value * b_range_multiplem,
+                for b in np.arange(-1*self.max_feature_value * b_range_multiple,
+                                   self.max_feature_value * b_range_multiple,
                                    step * b_multiple):
                     for transformation in transforms:
                         w_t = w * transformation
                         found_option = True
-
-                        for i in self.y:
+                        
+                        for i in self.data:
                             for xi in self.data[i]:
-                                yi = i
-                                if not yi * (np.dot(w_t, xi)) >= 1:
-                                    found_option = False
+                                yi=i
+                                if not yi*(np.dot(w_t,xi) + b )>=1:
+                                    found_option=False
                         if found_option:
-                            opt_dict[np.linalg.norm(w_t)] = [w_t, b]
+                            print("XD")
+                            opt_dict[np.linalg.norm(w_t)] = [w_t,b]
                 
                 if w[0] < 0:
-                    optimized = True
-                    print("optimized a step!")
+                    optimized=True
+                    print("Optimized a step!")
                 else:
                     w = w - step
-        
-        norms = sorted([n for n in opt_dict])
-        opt_choice = opt_dict[norms[0]]
+                    
+            norms = sorted([n for n in opt_dict])
+            opt_choice = opt_dict[norms[0]]
 
-        self.w = opt_choice[0]
-        self.b = opt_choice[1]
-
-        latest_optimum = opt_choice[0][0] + step * 2
+            self.w=opt_choice[0]
+            self.b=opt_choice[1]
+            
+            latest_optimum = opt_choice[0][0] + step * 2
     
-    def predict(self, features):
-        classification = np.sign(np.dot(np.array(features),self.w) + self.b)
-        if classification != 0 and self.visualization:
+    def predict(self,features):
+        classification = np.sign(np.dot(np.array(features),self.w)+self.b)
+        if classification!=0 and self.visualization:
             self.ax.scatter(features[0],features[1],s=200,marker='*',c=self.colors[classification])
         return (classification,np.dot(np.array(features),self.w)+self.b)
+    
+    def visualize(self):
+        [[self.ax.scatter(x[0],x[1],s=100,c=self.colors[i]) for x in data_dict[i]] for i in data_dict]
+        
+        def hyperplane(x,w,b,v):
+            return (-w[0] * x - b + v)/w[1]
+       
+        hyp_x_min= self.min_feature_value*0.9
+        hyp_x_max = self.max_feature_value*1.1
+        
+        pav1 = hyperplane(hyp_x_min,self.w,self.b,1)
+        pav2 = hyperplane(hyp_x_max,self.w,self.b,1)
+        self.ax.plot([hyp_x_min,hyp_x_max],[pav1,pav2],'k')
+        
+        nav1 = hyperplane(hyp_x_min,self.w,self.b,-1)
+        nav2 = hyperplane(hyp_x_max,self.w,self.b,-1)
+        self.ax.plot([hyp_x_min,hyp_x_max],[nav1,nav2],'k')
+        
+        db1 = hyperplane(hyp_x_min,self.w,self.b,0)
+        db2 = hyperplane(hyp_x_max,self.w,self.b,0)
+        self.ax.plot([hyp_x_min,hyp_x_max],[db1,db2],'y--')
+        plt.show()
